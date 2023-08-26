bc6b91dd62f6c1b1294cc8347c02854390cd1873
Rkbarda <ravikumar0896@gmail.com>
2023-08-26 10:02:38 +0530

    Create Basic.dart

---
+++ Basic.dart
@@ -0,0 +1,20 @@
+void main() {
+  var a = "ravi";
+  var b = "kumar";
+  print("$a $b");
+  for (double i = 1.5; i < 5; i++) {
+    print('hello ${i + 1}');
+  }
+  String name;
+  name = "Paulo";
+  print(name);
+  var isTrue = true;
+  print(isTrue);
+  const r = 8.7;
+  print("hello there ${a.toUpperCase()} ${b.toUpperCase()},you got $r points");
+  if (!(a is! String)) {
+    print("hlwwwwww");
+  } else {
+    print("noooooooooo");
+  }
+}
